fc04cfe9fa5b94450ede118379e9c7af6dab1ca6
JacobTaylorNWMSU <s526858@nwmissouri.edu>
2019-10-14 12:38:11 -0500

    Created Repo and added files

---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require(http)
+
+const hostname = 0.0.0.0;
+const port = 3002;
+
+const server = http.createServer((req, res)) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    res.write('Hello...\n')
+    res.write('Hello...\n')
+    res.end('Hello World! This is text - we can respond with HTML, JSON, and more \n')
+}
+server.listen(port, hostname, () => {
+    console.log("Server running at http//")
+})
+
+//node server.js in powershell
